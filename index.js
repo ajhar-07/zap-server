93a6e0148537913c7b2180ff47375c175c29e6e6
ajhar-07 <fahimajhar737@gmail.com>
2025-12-20 17:40:45 +0600

    Delete added

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const express=require('express')
 const cors=require('cors')
 require('dotenv').config()
 const port=process.env.PORT||3000
-const { MongoClient, ServerApiVersion, Db } = require('mongodb');
+
+// function generateTrackingId() {
+//   const PREFIX = "PRC"; 
+//   const COUNTRY = "BD"; 
+
+//   const now = new Date();
+//   const date =
+//     now.getFullYear().toString() +
+//     String(now.getMonth() + 1).padStart(2, "0") +
+//     String(now.getDate()).padStart(2, "0");
+
+//   const random = Math.floor(100000 + Math.random() * 900000);
+
+//   return `${PREFIX}-${COUNTRY}-${date}-${random}`;
+// }
+
+
+const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
 const app=express()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufivfja.mongodb.net/?appName=Cluster0`;
 
@@ -32,19 +49,29 @@ async function run() {
 
     app.post('/parcels', async(req,res)=>{
         const newParcel=req.body
+        newParcel.createdAt=new Date()
         const result=await parcelsCollection.insertOne(newParcel)
         res.send(result)
     })
 
+  app.delete('/parcels/:id', async(req,res)=>{
+    const id=req.params.id
+    const query={_id: new ObjectId(id)}
+    const result= await parcelsCollection.deleteOne(query)
+    res.send(result)
+  })
+
+
    app.get('/parcels', async (req, res) => {
   const email = req.query.email
+  const options={sort:{createdAt:-1}}
   let query = {}
 
   if (email) {
     query = { senderEmail: email }
   }
 
-  const result = await parcelsCollection.find(query).toArray()
+  const result = await parcelsCollection.find(query,options).toArray()
   res.send(result)
 })
 
